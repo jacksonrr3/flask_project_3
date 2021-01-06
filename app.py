6b8936c19f0105f5fc3eb389b6730fc1cb59aa66
Evgeniy <jacksonrr3@yandex.ru>
2021-01-06 23:20:43 +0300

    query filter goals

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from flask import Flask, render_template, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm, csrf
 from flask_migrate import Migrate
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import InputRequired, Email, AnyOf, Regexp, Length
 
-from data import goals, days
+from data import days
 
 
 app = Flask(__name__)
@@ -79,6 +80,17 @@ class Request(db.Model):
 # db.create_all()
 
 
+#class RequestForm(FlaskForm):
+    #name =
+
+
+class BookingForm(FlaskForm):
+    name = StringField("Вас зовут",
+                       validators=[InputRequired()])
+    phone = StringField("Ваш телефон",
+                        validators=[InputRequired()])
+
+
 def read_data_from_json_file(path):
     """ reading data from data_base json file, return dict """
     try:
@@ -97,11 +109,16 @@ def write_data_to_json_file(path, data):
         print("An IOError has occurred!")
 
 
+@app.errorhandler(404)
+def render_not_found(error):
+    return 'Ничего не нашлось! Вот неудача, отправляйтесь на главную!\n<a href="/">TINYSTEPS</a>'
 
+# done
 @app.route('/')
 def render_index():
-    teachers = read_data_from_json_file('data_base.json')
+    teachers = db.session.query(Teacher).all()
     ch_teachers = random.sample(teachers, 6)
+    goals = db.session.query(Goal).all()
     return render_template('index.html',
                            teachers=ch_teachers,
                            goals=goals)
@@ -109,9 +126,10 @@ def render_index():
 
 @app.route('/all/', methods=['POST', 'GET'])
 def render_all():
-    teachers = read_data_from_json_file('data_base.json')
+    teachers = db.session.query(Teacher).all()
     selected_value = request.args.get('selected')
     if selected_value == '1':
+        teachers = teachers.all()
         random.shuffle(teachers)
     elif selected_value == '2':
         teachers.sort(key=operator.itemgetter("rating"), reverse=True)
@@ -122,52 +140,56 @@ def render_all():
     else:
         random.shuffle(teachers)
     return render_template('all.html',
-                           teachers=teachers,
-                           goals=goals)
+                           teachers=teachers)
+                           #goals=goals)
 
 
 @app.route('/goals/<goal>/')
 def render_goal(goal):
-    teachers = read_data_from_json_file('data_base.json')
-    teachers.sort(key=operator.itemgetter("rating"), reverse=True)
+    #teachers = read_data_from_json_file('data_base.json')
+    #teachers.sort(key=operator.itemgetter("rating"), reverse=True)
+    teachers = db.session.query(Teacher).filter(Teacher.goals.any(goal)).order_by(Teacher.rating.desc())
+    goal = db.session.query(Goal).filter(Goal.name == goal).first().value
     return render_template('goal.html',
                            goal=goal,
-                           goals=goals,
+                           #goals=goals,
                            teachers=teachers)
 
-
+# done
 @app.route('/profiles/<int:teacher_id>/')
 def render_teacher(teacher_id):
-    teachers = read_data_from_json_file('data_base.json')
-    if teacher_id >= len(teachers):
-        abort(404)
-    teacher = teachers[teacher_id]
+    teacher = db.session.query(Teacher).get_or_404(teacher_id)
+    free = json.loads(teacher.free)
     return render_template('profile.html',
                            teacher=teacher,
-                           goals=goals,
+                           free=free,
                            days=days)
 
 
-@app.route('/request/')
-def render_request():
+#@app.route('/request/')
+#def render_request():
+#    return render_template('request.html')
+
+
+@app.route('/request/', methods=['GET', 'POST'])
+def route_request():
+    if request.method == "POST":
+        goal = request.form.get("goal")
+        time = request.form.get("time")
+        name = request.form.get("name")
+        phone = request.form.get("phone")
+        req = Request(name=name, phone=phone, time=time, goal=goal)
+        db.session.add(req)
+        db.session.commit()
+        #requests = read_data_from_json_file('request.json')
+        #requests.append(req)
+        #write_data_to_json_file('request.json', requests)
+        return render_template('request_done.html',
+                               goal=goal,
+                               time=time,
+                               name=name,
+                               phone=phone)
     return render_template('request.html')
-
-
-@app.route('/request_done/', methods=['POST'])
-def route_request_done():
-    goal = request.form.get("goal")
-    time = request.form.get("time")
-    name = request.form.get("name")
-    phone = request.form.get("phone")
-    req = {'goal': goal, 'time': time, 'name': name, 'phone': phone}
-    requests = read_data_from_json_file('request.json')
-    requests.append(req)
-    write_data_to_json_file('request.json', requests)
-    return render_template('request_done.html',
-                           goal=goal,
-                           time=time,
-                           name=name,
-                           phone=phone)
 
 
 @app.route('/booking/<int:teacher_id>/<day>/<time>/')
@@ -207,17 +229,21 @@ def route_booking_done():
                            client_phone=client_phone)
 
 
-# if __name__ == '__main__':
+#if __name__ == '__main__':
 #    app.run('0.0.0.0', 8000)
 
 
 # test db
 
 
-teachers = db.session.query(Teacher).all()
-for teacher in teachers:
-    goals = []
-    for goal in teacher.goals:
-        goals.append(goal.value)
-    print(teacher.id, teacher.name, teacher.picture, teacher.price, goals, teacher.free)
-    print('--------------------------------------------------------------------------------')
+#teachers = db.session.query(Teacher).all()
+#print(type(teachers[0].goals))
+#for teacher in teachers:
+    #goals = []
+    #for goal in teacher.goals:
+        #goals.append(goal.value)
+    #print(teacher.id, teacher.name, teacher.picture, teacher.price, goals, teacher.free)
+    #print('--------------------------------------------------------------------------------')
+
+
+
